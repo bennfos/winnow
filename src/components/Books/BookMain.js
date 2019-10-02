f683642040f83ede329f8765527bde7c416bc19d
Bennett Foster <bennfos@gmail.com>
2019-10-02 09:17:44 -0500

    refactored to change background on components correctly

--- src/components/Books/BookMain.js
+++ src/components/Books/BookMain.js
@@ -4,13 +4,6 @@ import BookList from "./BookList"
 
 class BookMain extends Component {
 
-componentDidMount () {
-    document.body.classList.add('bk2')
-}
-
-componentWillUnmount () {
-    document.body.classList.remove('bk2')
-}
 
     render() {
         return (
